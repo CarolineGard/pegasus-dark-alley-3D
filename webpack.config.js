a4aea5d6f05c0f9181a0b3ab379b50d2144983f8
johanhenrikssn <johan.c.henriksson@gmail.com>
2019-09-28 18:07:12 +0200

    Upgrade dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path            = require('path');
-var UglifyJsPlugin  = require('uglifyjs-webpack-plugin');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'production',
@@ -9,28 +10,33 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'bin')
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            PIXI: 'pixi.js'
+        })
+    ],
     module: {
-        rules: 
-        [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 
-                [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(glsl|vs|fs|frag|vert)$/,
-                loader: 'shader-loader',
-                options: {
-                    glsl: {
-                        chunkPath: path.resolve("/glsl/chunks")
+        rules:
+            [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use:
+                        [
+                            {
+                                loader: 'babel-loader'
+                            }
+                        ]
+                },
+                {
+                    test: /\.(glsl|vs|fs|frag|vert)$/,
+                    loader: 'shader-loader',
+                    options: {
+                        glsl: {
+                            chunkPath: path.resolve("/glsl/chunks")
+                        }
                     }
                 }
-            }
-        ]
+            ]
     }
 };
