1d7b73bd4bdd60d4abe6d8fdc5734dd80a188e8c
carolinegard <caroline.gard92@gmail.com>
2019-09-29 19:27:26 +0200

    First setup Babylon

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import createScene from "./createScene.js";
+
+var canvas = document.getElementById("renderCanvas"); // Get the canvas element
+var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+/******* End of the create scene function ******/
+var scene = createScene(engine, canvas); //Call the createScene function
+
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function() {
+  scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function() {
+  engine.resize();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,32 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-    devtool: 'module-source-map',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('build'),
-        filename: 'index.bundle.js',
-        publicPath: '/',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
-        new webpack.ProvidePlugin({
-            PIXI: 'pixi.js'
-        })
-    ],
-    module: {
-        rules:
-            [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use:
-                        [
-                            {
-                                loader: 'babel-loader'
-                            }
-                        ]
-                },
-                {
-                    test: /\.(glsl|vs|fs|frag|vert)$/,
-                    loader: 'shader-loader',
-                    options: {
-                        glsl: {
-                            chunkPath: path.resolve("/glsl/chunks")
-                        }
-                    }
-                }
-            ]
-    }
+  devtool: "module-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("build"),
+    filename: "index.bundle.js",
+    publicPath: "/"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      }
+    ]
+  }
 };
