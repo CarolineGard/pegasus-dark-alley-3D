8568494d23c5c70078439d1ad469632b164bafa3
carolinegard <caroline.gard92@gmail.com>
2019-12-01 21:41:45 +0100

    Add menu file

---
+++ src/game/GuiMenu.js
@@ -0,0 +1,22 @@
+import * as GUI from "@babylonjs/gui";
+
+const GuiMenu = (scene, player) => {
+  let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+  let pointsText = new GUI.TextBlock();
+  pointsText.text = player.getPoints().toString();
+  pointsText.color = "grey";
+  pointsText.fontSize = 24;
+  pointsText.left = "-40px";
+  pointsText.top = "20px";
+  pointsText.textHorizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+  pointsText.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+
+  advancedTexture.addControl(pointsText);
+
+  scene.registerBeforeRender(() => {
+    pointsText.text = player.getPoints().toString();
+  });
+};
+
+export default GuiMenu;
