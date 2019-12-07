70b5eb105e6bb9f202c0fd9a1263cac68774b39a
Johan Henriksson <johan.c.henriksson@gmail.com>
2019-12-07 12:32:46 +0100

    Rename GUI to HUD

--- src/createScene.js
+++ src/createScene.js
@@ -7,33 +7,18 @@ import Player from "./game/Player";
 import Level from "./game/Level";
 import SceneEffects from "./game/SceneEffects";
 import Trees from "./game/Trees";
-import Gui from "./game/Gui";
+import Hud from "./game/Hud";
+import GuiMenu from "./game/GuiMenu";
 class Game {
+  startGame(scene, level, player) {
+    level.resetLevel();
+    player.setup(scene);
+    Hud(scene, player);
+  }
 
-  constructor() {
-    this.gameStarted = false;
-  }
-  
-  startGame() {
-    console.log("gameStart");
-    console.log({"game": this.gameStarted});
-    this.gameStarted = true;
-    this.CreateScene();
-  }
   /******* Add the create scene function ******/
   CreateScene(engine, canvas) {
     let scene = new BABYLON.Scene(engine);
-
-    console.log({"gamesTart": this.gameStarted});
-    let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-    let button = GUI.Button.CreateSimpleButton("startButton", "Click to Play");
-
-
-
-   
-
-    console.log("post gameStarted");
-    
 
     // Reduce calls to gl.clear() by disable the default scene clearing behavior
     // Safe setting since the viewport will always be 100% filled (inside skybox)
@@ -46,52 +31,15 @@ class Game {
 
     SkyBox(scene);
     let level = new Level();
+    let player = new Player();
     level.setup(scene);
 
     SceneEffects(scene);
     Trees(scene);
-    if (this.gameStarted === true) {
-      
- 
- 
-     //  let scene1 = new BABYLON.Scene(engine);
-     // let camera1 = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene1);
-   
-     //  // Make this scene transparent to see the document background
-     //  //scene1.clearColor = new BABYLON.Color4(0,0,0,0);
-    
-     //  let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-     //  let button = GUI.Button.CreateSimpleButton("startButton", "Click to Play");
-     //    button.width = "150px"
-     //    button.height = "40px";
-     //    button.color = "white";
-     //    button.cornerRadius = 20;
-     //    button.background = "purple";
-     //    button.onPointerUpObservable.add(() => {
-     //     this.gameStarted = true;
-     //     this.CreateScene();
-     //    });
-     //    advancedTexture.addControl(button);    
-      
-     //    return scene1;
-    } else {
-        button.width = "150px"
-        button.height = "40px";
-        button.color = "white";
-        button.cornerRadius = 20;
-        button.background = "purple";
-        button.onPointerUpObservable.add(() => {
-          advancedTexture.removeControl(button);    
-          level.resetLevel();
-          let player = new Player();
-          player.setup(scene);
-          Gui(scene, player);
-        });
-        advancedTexture.addControl(button);    
-    }
+    GuiMenu(scene, level, player, this.startGame);
 
     return scene;
-  };
+  }
 }
 
 export default Game;
--- src/game/GuiMenu.js
+++ src/game/GuiMenu.js
@@ -1,35 +1,18 @@
 import * as GUI from "@babylonjs/gui";
 
-const GuiMenu = (scene, startGame) => {
-  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-
-  // Make this scene transparent to see the document background
-  scene.clearColor = new BABYLON.Color4(0,0,0,0);
-
+const GuiMenu = (scene, level, player, startGame) => {
   let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
   let button = GUI.Button.CreateSimpleButton("startButton", "Click to Play");
-    button.width = "150px"
-    button.height = "40px";
-    button.color = "white";
-    button.cornerRadius = 20;
-    button.background = "green";
-    button.onPointerUpObservable.add(() => startGame());
-    advancedTexture.addControl(button);    
-  // let pointsText = new GUI.TextBlock();
-  // pointsText.text = player.getPoints().toString();
-  // pointsText.color = "grey";
-  // pointsText.fontSize = 24;
-  // pointsText.left = "-40px";
-  // pointsText.top = "20px";
-  // pointsText.textHorizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
-  // pointsText.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
-
-  // advancedTexture.addControl(pointsText);
-
-  // scene.registerBeforeRender(() => {
-  //   pointsText.text = player.getPoints().toString();
-  // });
+  button.width = "150px";
+  button.height = "40px";
+  button.color = "#c9c9c9";
+  button.cornerRadius = 20;
+  button.background = "black";
+  button.onPointerUpObservable.add(() => {
+    advancedTexture.removeControl(button);
+    startGame(scene, level, player);
+  });
+  advancedTexture.addControl(button);
 };
 
 export default GuiMenu;
