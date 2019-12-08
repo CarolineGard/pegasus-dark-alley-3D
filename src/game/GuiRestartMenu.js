98779260515790e6717b1fc408f4f60242086921
Johan Henriksson <johan.c.henriksson@gmail.com>
2019-12-08 11:31:45 +0100

    Fix restart button i RestartGui

--- src/game/GuiRestartMenu.js
+++ src/game/GuiRestartMenu.js
@@ -1,25 +1,30 @@
 import * as GUI from "@babylonjs/gui";
 
-const GuiRestartMenu = (scene, level, player, startGame) => {
+const GuiRestartMenu = (scene, level, player, trees, engine, startGame) => {
   let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
   let button = GUI.Button.CreateSimpleButton("startButton", "Restart");
-  var text1 = new GUI.TextBlock();
-  text1.text = "Your score was " + player.getPoints();
-  text1.color = "white";
-  text1.fontSize = 24;
+  var text = new GUI.TextBlock();
+  text.text = "Your score was " + player.getPoints();
+  text.color = "white";
+  text.fontSize = 24;
+  text.top = "-70px";
 
-  button.top = "70px";
   button.width = "150px";
   button.height = "40px";
   button.color = "#c9c9c9";
   button.cornerRadius = 20;
   button.background = "black";
+
+  console.log("set gui");
+
   button.onPointerUpObservable.add(() => {
+    console.log("onPointerObservable");
+    advancedTexture.removeControl(text);
     advancedTexture.removeControl(button);
-    startGame(scene, level, player);
+    startGame(scene, level, player, trees, engine);
   });
+  advancedTexture.addControl(text);
   advancedTexture.addControl(button);
-  advancedTexture.addControl(text1);
 };
 
 export default GuiRestartMenu;
