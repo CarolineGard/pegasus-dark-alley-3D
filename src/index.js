666572553a5e034efdc5a49bdef251303e4a5ec4
Johan Henriksson <johan.c.henriksson@gmail.com>
2019-12-19 21:09:38 +0100

    Remove comments

--- src/index.js
+++ src/index.js
@@ -8,13 +8,6 @@ let engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 let game = new Game(engine);
 game.createInitialScene(engine, canvas); //Call the createScene function
 
-// Register a render loop to repeatedly render the scene
-// scene.executeWhenReady(() => {
-// engine.runRenderLoop(() => {
-//   scene.render();
-// });
-// });
-
 // Watch for browser/canvas resize events
 window.addEventListener("resize", function() {
   engine.resize();
